b38dd799fd11511db7f42685d03ffa77ddc414b8
Pasindu20020817 <chethiyawijesingha17@gmail.com>
2026-02-18 17:28:40 +0530

    Create a web server

--- app.js
+++ app.js
@@ -7,7 +7,17 @@ fs.readFile('file.txt', 'utf8', function (err, data) {   if (err) throw err;
 }); 
 
 // Writing to a file using Node.js
-const fs = require('fs'); 
+ 
 fs.writeFile('file.txt', 'Hello World!', function (err) {   if (err) throw err; 
   console.log('File saved!'); 
 }); 
+
+
+// Creating a simple HTTP server using Node.js
+const http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
