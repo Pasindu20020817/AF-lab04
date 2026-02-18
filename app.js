42586089fecc6ed8a2dc7546c2b38522520b6924
Pasindu20020817 <chethiyawijesingha17@gmail.com>
2026-02-18 17:50:01 +0530

    Making an HTTP request

--- app.js
+++ app.js
@@ -21,3 +21,18 @@ http.createServer(function (req, res) {
   res.write('Hello World!');
   res.end();
 }).listen(8080);
+
+// Making an HTTP request using Node.js
+const https = require('https'); 
+https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {   let data = ''; 
+ 
+  resp.on('data', (chunk) => { 
+    data += chunk; 
+  }); 
+ 
+  resp.on('end', () => { 
+    console.log(JSON.parse(data)); 
+  }); 
+}).on('error', (err) => { 
+  console.log("Error: " + err.message); 
+}); 
